eaacd87ad910f79fefb2cd66bea2fd9b3932d5e3
francescocastelli <francesco8.castelli@mail.polimi.it>
2022-01-04 12:37:52 +0100

    add cptk to run

--- src/run.sh
+++ src/run.sh
@@ -5,11 +5,13 @@
 name="las"
 workers_num=4
 bs=32
-lr=0.2
+lr=0.02
 l2=0.0
 lr_decay=0.98
-epochs=30
+epochs=150
 seed=1000
+ckpt_rate=0
+embeddings_num=0
 
 # front end parameters
 win_len_s=0.025
@@ -25,9 +27,9 @@ power=2
 
 console_logs=true
 #logs=true
+#ckpt=true
 # distributed=true
 # multitrain=true
-# embeddings=true
 
 # train command
 
@@ -35,6 +37,7 @@ python3 train.py --name ${name} -w ${workers_num} --bs ${bs} --lr ${lr} \
 		 --lr_decay ${lr_decay} --epochs ${epochs} --l2 ${l2} --seed ${seed} \
 		 --win_len_s ${win_len_s} --hop_len_s ${hop_len_s} --sr ${sr} \
 		 --f_min ${f_min} --f_max ${f_max} --pad ${pad} --n_mels ${n_mels} --power ${power} \
-		 ${console_logs:+--console_logs} ${logs:+--logs} \
+		 --embeddings_num ${embeddings_num} \
+		 ${console_logs:+--console_logs} ${logs:+--logs} ${ckpt:+--ckpt} --ckpt_rate ${ckpt_rate}\
 		 ${distributed:+--distributed} ${embeddings:+--embeddings} ${multitrain:+--multitrain}
 
