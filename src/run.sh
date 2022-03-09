37ad527de0d7d919018fad7781c87647d63339c2
francescocastelli <francesco8.castelli@mail.polimi.it>
2022-03-09 09:24:55 +0100

    update run file

--- src/run.sh
+++ src/run.sh
@@ -7,11 +7,17 @@ workers_num=4
 bs=32
 lr=0.02
 l2=0.0
-lr_decay=0.98
-epochs=150
+lr_decay=0.99
+sampling_rate=0.01
+epochs=501
 seed=1000
-ckpt_rate=0
+ckpt_rate=50
 embeddings_num=0
+device="cuda:0"
+
+# load chekpoint
+#ckpt_dir="12-01-22_10:47:20"
+#ckpt_name="las_ckpt_epoch_150"
 
 # front end parameters
 win_len_s=0.025
@@ -25,19 +31,21 @@ power=2
 
 # optional parameters, comments a line if you dont want it
 
-console_logs=true
-#logs=true
-#ckpt=true
-# distributed=true
-# multitrain=true
+#console_logs=true
+logs=true
+#distributed=true
+#multitrain=true
 
 # train command
+if [ -z ${device+d} ]; then device=""; else device="-d "$device ; fi
+if [ -z ${ckpt_dir+ckpt_dir} ]; then ckpt_dir=""; else ckpt_dir="--ckpt_dir "$ckpt_dir; fi
+if [ -z ${ckpt_name+ckpt_name} ]; then ckpt_name=""; else ckpt_name="--ckpt_name "$ckpt_name; fi
 
-python3 train.py --name ${name} -w ${workers_num} --bs ${bs} --lr ${lr} \
+python3 train.py --name ${name} -w ${workers_num} --bs ${bs} --lr ${lr} --sampling_rate ${sampling_rate} \
 		 --lr_decay ${lr_decay} --epochs ${epochs} --l2 ${l2} --seed ${seed} \
 		 --win_len_s ${win_len_s} --hop_len_s ${hop_len_s} --sr ${sr} \
 		 --f_min ${f_min} --f_max ${f_max} --pad ${pad} --n_mels ${n_mels} --power ${power} \
-		 --embeddings_num ${embeddings_num} \
+		 --embeddings_num ${embeddings_num} ${device} ${ckpt_dir} ${ckpt_name} \
 		 ${console_logs:+--console_logs} ${logs:+--logs} ${ckpt:+--ckpt} --ckpt_rate ${ckpt_rate}\
 		 ${distributed:+--distributed} ${embeddings:+--embeddings} ${multitrain:+--multitrain}
 
